892c765ecad0cb7f090133b5eda420bad7970220
marc <marc1ouellet0@gmail.com>
2025-06-19 20:22:47 -0400

    a

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -48,31 +48,33 @@ void graphics::render(simple_object* cube, Image* image) {
 
       float iminx = math::clamp(mmin(mmin(trig.a.x, trig.b.x), trig.c.x), 0, image->x);
       float iminy = math::clamp(mmin(mmin(trig.a.y, trig.b.y), trig.c.y), 0, image->y);
+
       float imaxx = math::clamp(mmax(mmax(trig.a.x, trig.b.x), trig.c.x), 0, image->x);
       float imaxy = math::clamp(mmax(mmax(trig.a.y, trig.b.y), trig.c.y), 0, image->y);
-      
-      //float minx = math::clamp(mmin(mmin(curtri.v.a.x, curtri.v.b.x), curtri.v.c.x), -SCREENSPACE_RANGE, SCREENSPACE_RANGE);
-      //float miny = math::clamp(mmin(mmin(curtri.v.a.y, curtri.v.b.y), curtri.v.c.y), -SCREENSPACE_RANGE, SCREENSPACE_RANGE);
-      //float maxx = math::clamp(mmax(mmax(curtri.v.a.x, curtri.v.b.x), curtri.v.c.x), -SCREENSPACE_RANGE, SCREENSPACE_RANGE);
-      //float maxy = math::clamp(mmax(mmax(curtri.v.a.y, curtri.v.b.y), curtri.v.c.y), -SCREENSPACE_RANGE, SCREENSPACE_RANGE);
+      /*
+      float minx = math::clamp(mmin(mmin(curtri.v.a.x, curtri.v.b.x), curtri.v.c.x), -SCREENSPACE_RANGE, SCREENSPACE_RANGE);
+      float miny = math::clamp(mmin(mmin(curtri.v.a.y, curtri.v.b.y), curtri.v.c.y), -SCREENSPACE_RANGE, SCREENSPACE_RANGE);
+      float maxx = math::clamp(mmax(mmax(curtri.v.a.x, curtri.v.b.x), curtri.v.c.x), -SCREENSPACE_RANGE, SCREENSPACE_RANGE);
+      float maxy = math::clamp(mmax(mmax(curtri.v.a.y, curtri.v.b.y), curtri.v.c.y), -SCREENSPACE_RANGE, SCREENSPACE_RANGE);
 
-      //int j_max = mmap(maxx, -SCREENSPACE_RANGE, SCREENSPACE_RANGE, 0, image->x) + 1;
-      //int k_max = mmap(maxy, -SCREENSPACE_RANGE, SCREENSPACE_RANGE, 0, image->y) + 1;
-      //for (int j = mmap(minx, -SCREENSPACE_RANGE, SCREENSPACE_RANGE, 0, image->x); j < j_max; j++) {
-      //  for (int k = mmap(miny, -SCREENSPACE_RANGE, SCREENSPACE_RANGE, 0, image->y); k < k_max; k++) {
+      int j_max = mmap(maxx, -SCREENSPACE_RANGE, SCREENSPACE_RANGE, 0, image->x) + 1;
+      int k_max = mmap(maxy, -SCREENSPACE_RANGE, SCREENSPACE_RANGE, 0, image->y) + 1;
+      for (int j = mmap(minx, -SCREENSPACE_RANGE, SCREENSPACE_RANGE, 0, image->x); j < j_max; j++) {
+        for (int k = mmap(miny, -SCREENSPACE_RANGE, SCREENSPACE_RANGE, 0, image->y); k < k_max; k++) {
+      */
       for (int j = iminx; j < imaxx; j++) {
         for (int k = iminy; k < imaxy; k++) {
-          screenposFx = (j * 0.005859375) - 3;
-          screenposFy = (k * 0.005859375) - 3;
-          screenposFx = mmap(j, 0, image->x, -SCREENSPACE_RANGE, SCREENSPACE_RANGE);
-          screenposFy = mmap(k, 0, image->y, -SCREENSPACE_RANGE, SCREENSPACE_RANGE);
-          //if (PointInTriangle(curtri.v, float2(screenposFx, screenposFy))) {
-          if (PointInTriangle(trig, float2(j, k))) {
-            image->pixels[j][k].r = curtri.color.x * 255;  // * math::max(0, curtri.n.y);
-            image->pixels[j][k].g = curtri.color.y;        // * math::max(0, curtri.n.y);
-            image->pixels[j][k].b = curtri.color.z;        // * math::max(0, curtri.n.y);
+          // screenposFx = (j * 0.005859375) - 3;
+          // screenposFy = (k * 0.005859375) - 3;
+          // screenposFx = mmap(j, 0, image->x, -SCREENSPACE_RANGE, SCREENSPACE_RANGE);
+          // screenposFy = mmap(k, 0, image->y, -SCREENSPACE_RANGE, SCREENSPACE_RANGE);
+          // if (PointInTriangle(curtri.v, float2(screenposFx, screenposFy))) {
+          if (!PointInTriangle(trig, float2(j, k)))continue;// {
+            image->pixels[j][k].r = curtri.color.x;  // * math::max(0, curtri.n.y);
+            image->pixels[j][k].g = curtri.color.y;  // * math::max(0, curtri.n.y);
+            image->pixels[j][k].b = curtri.color.z;  // * math::max(0, curtri.n.y);
             // image->pixels[j][k] = curtri.color;  // * math::max(0, curtri.n.y);
-          }
+          //}
         }
       }
     }
